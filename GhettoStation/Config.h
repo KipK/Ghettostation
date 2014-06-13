f1f11f555c7e46c7520b9f3cadf6706d74be23d2
osmogr <gosmondson@gmail.com>
2014-06-13 11:02:17 -0700

    Added defines and structure for Graphical LCD screen

--- GhettoStation/Config.h
+++ GhettoStation/Config.h
@@ -60,6 +60,11 @@
 #define I2CADRESS 0x27 // LCD03/05 have 0x63 or 0xc6 ( even if it's written 0xc6 when powering the lcd03, in fact it uses 0x63 so try booth)
                         // LCM1602 uses 0x27 & GY-LCD use 0x20
 
+// Graphical LCD - Using system5x7 font so its nearly 20x4 size
+// Uncomment the below line and comment out the above LCD defines
+
+//#define GLCDEnable
+
 //#################################### SERVOS ENDPOINTS #############################################################
 // NO NEED TO EDIT THIS
 //. Those are just default values when not configured.
@@ -101,7 +106,7 @@
   #define ENTER_BUTTON_PIN 36   //Any Digital pin
   #define ADC_VOLTAGE      41   //(A5) ADC pin used for voltage reading
   #define BUZZER_PIN        8   //(PH5) Any PWM pin ((add a 100-150 ohm resistor between buzzer & ground)
-#endif		
+#endif
 
 //################################################## DEBUG ##########################################################
 //#define DEBUG
